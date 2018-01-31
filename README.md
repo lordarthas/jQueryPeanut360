33db8196bf4768cbcb7449ec2093cce1f087a896
Michele Beltrame <mb@italpro.net>
2018-01-31 16:23:53 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # jQueryPeanut360
-360-degrees image animator/slider which doesn't get in the way
+
+360-degrees image animator/slider which doesn't get in your way.
+
+## Key features
+
+This is a small jQuery plugin for creating 360-degreems image sliders. It doesn't try to do more than it needs to, but it instead exposes the methods which allow you to do anything you want.
+
+These are the key points behind this plugin:
+
+ - It just analyzes your img tags and show/hides them when necessary
+ - It doesn't modify the DOM
+ - It doesn't care if you wrap one or more of your images in another tag (such as and <a> to view a zoom)
+ - It doesn't force you to use any provided UI or button, you just use your own
+ - It just needs jQuery to work
+
+## TODO
+
+ - Handle mobile swipe events to turn slides
---
+++ js/jquery.peanut360.js
@@ -0,0 +1,107 @@
+/*
+  jQuery Peanut360
+  Version 1.0.0
+*/
+
+(function ( $ ) {
+    "use strict";
+    function Peanut360(args) {
+        this.$node = args.$node;
+        this.$slides = this.$node.find(".p360slide");
+        var $currentslide = this.$node.find(".p360slide:not(.d-none)");
+        this.csidx = this.$slides.index($currentslide);
+        this.lsidx = this.$slides.length - 1;
+
+        if ( args.nextonclick ) {
+            this.$node.on("click", $.proxy(function() {
+                if ( !this.intervalid ) {
+                  this.shownext();
+                }
+            }, this));
+        }
+    }
+
+    Peanut360.prototype = {
+        $node           : null,
+        // We need an array of the slides because we need to cycle between
+        // them regardless if they're wrapped individually in <a> or other tags
+        $slides         : null,
+        csidx           : null,
+        lsidx           : null,
+        interval        : 200,
+        intervalid      : null,
+
+        animate : function() {
+            if ( this.intervalid ) {
+              this.animate_stop();
+            }
+            this.intervalid = window.setInterval($.proxy(function() {
+              this.shownext();
+            }, this), this.interval);
+        },
+
+        animate_reverse : function() {
+            if ( this.intervalid ) {
+              this.animate_stop();
+            }
+            this.intervalid = window.setInterval($.proxy(function() {
+              this.showprevious();
+            }, this), this.interval);
+        },
+
+        shownext : function() {
+          $( (this.$slides)[this.csidx] ).addClass("d-none");
+          this.csidx = (this.csidx < this.lsidx) ? this.csidx+1 : 0;
+          var $nextslide = $( this.$slides[this.csidx] );
+          $nextslide.removeClass("d-none");
+        },
+
+        showprevious : function() {
+          $( (this.$slides)[this.csidx] ).addClass("d-none");
+          this.csidx = (this.csidx > 0) ? this.csidx-1 : this.lsidx;
+          var $prevslide = $( this.$slides[this.csidx] );
+          $prevslide.removeClass("d-none");
+        },
+
+        animate_stop : function() {
+          if ( this.intervalid ) {
+            window.clearInterval(this.intervalid);
+            this.intervalid = null;
+          }
+        }
+    };
+
+    $.fn.peanut360 = function(args) {
+      switch(args) {
+          default:
+            return this.each(function() {
+                if (!args) { args = {}; }
+                args.$node = $(this);
+                var peanut360 = new Peanut360(args);
+                $(this).data("peanut360", peanut360);
+            });
+          case "animate":
+              return this.each(function() {
+                  $(this).data("peanut360").animate();
+              });
+          case "animate_reverse":
+              return this.each(function() {
+                  $(this).data("peanut360").animate_reverse();
+              });
+          case "animate_stop":
+              return this.each(function() {
+                  $(this).data("peanut360").animate_stop();
+              });
+          case "showprevious":
+              return this.each(function() {
+                  $(this).data("peanut360").animate_stop();
+                  $(this).data("peanut360").showprevious();
+              });
+          case "shownext":
+              return this.each(function() {
+                  $(this).data("peanut360").animate_stop();
+                  $(this).data("peanut360").shownext();
+              });
+        };
+    };
+}( jQuery ));
